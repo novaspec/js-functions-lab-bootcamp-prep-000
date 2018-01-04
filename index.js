6827b2b1cee04d7179e6b1a076fc06b37fc8c64f
Jonatan Kaplan <jonatankaplan@gmail.com>
2018-01-04 19:16:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,14 @@
 function happyHolidays(string){
   return 'Happy Holidays!'
 };
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`;
+};
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+};
+
+function holidayCountdown(holiday, number) {
+  return `It\'s ${number} days until ${holiday}!`;
+};
